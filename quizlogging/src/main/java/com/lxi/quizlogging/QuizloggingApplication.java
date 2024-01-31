b22ac7a44754da38d04454405f5e4727b5e480f6
yadu-krishnan7 <yadhuvasanthigovindan7@gmail.com>
2024-01-31 16:00:45 +0530

    added another logging service as consumer

---
+++ quizlogging/src/main/java/com/lxi/quizlogging/QuizloggingApplication.java
@@ -0,0 +1,13 @@
+package com.lxi.quizlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizloggingApplication.class, args);
+	}
+
+}
---
+++ quizlogging/src/test/java/com/lxi/quizlogging/QuizloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxi.quizlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
